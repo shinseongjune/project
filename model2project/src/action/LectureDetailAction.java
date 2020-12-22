368dd80370263c56fa21bf048c4f2e40f3dffa28
sodaus145 <shinseongjune.dev@gmail.com>
2020-12-22 09:16:46 +0900

    201222 오타 수정

--- model2project/src/action/LectureDetailAction.java
+++ model2project/src/action/LectureDetailAction.java
@@ -6,12 +6,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import svc.LectureLastPageService;
-import svc.LectureListService;
-import svc.SubjectListService;
 import vo.ActionForward;
+import vo.Lecture_Video;
 import vo.Member;
-import vo.Subject;
 
 public class LectureDetailAction implements Action {
 
@@ -25,7 +22,7 @@ public class LectureDetailAction implements Action {
 			int lecture_num = Integer.parseInt(request.getParameter("lecture_num"));
 			
 			LectureDetailListService lectureDetailListService = new LectureDetailListService();
-			LinkedList<Lecture_video> vidList = lectureDetailListService.getVid(lecture_num);
+			LinkedList<Lecture_Video> vidList = lectureDetailListService.getVid(lecture_num);
 			//챕터(순서대로) -> 유튜브주소랑 챕터타이틀 담아오기
 			session.setAttribute("vidList", vidList);
 			forward.setPath("lecture_Detail.jsp?lecture_num=" + lecture_num);
